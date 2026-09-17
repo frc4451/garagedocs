a02a523f31a8408cca60d0117f1c49f5fb5d115d
Judson James <judsonjames96@gmail.com>
2026-09-17 16:30:03 -0400

    Images, first pass

--- src/mdx-components.ts
+++ src/mdx-components.ts
@@ -12,6 +12,7 @@ import LogicalOperators from '@/components/blocks/LogicalOperators.astro';
 import ContentTable from '@/components/blocks/ContentTable.astro';
 import TextBlock from '@/components/blocks/TextBlock.astro';
 import YouTubeEmbed from '@/components/blocks/YouTubeEmbed.astro';
+import ReferenceImage from '@/components/blocks/ReferenceImage.astro';
 import Sources from '@/components/blocks/Sources.astro';
 import JavaPlayground from '@/components/java-playground/JavaPlayground.astro';
 
@@ -19,6 +20,7 @@ export const components: MDXComponents = {
   TextBlock,
   JavaPlayground,
   YouTubeEmbed,
+  ReferenceImage,
   RulesBox,
   StepsBox,
   CodeBlock,
